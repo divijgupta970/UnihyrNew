1f1f23fbb1cdded262f6578d71c0d55f5a7f0f8a
Divij Gupta <divijgupta66@gmail.com>
2019-12-12 01:40:24 +0530

    initial commit

--- app/src/main/java/com/unihyr/Unihyr/MainActivity.java
+++ app/src/main/java/com/unihyr/Unihyr/MainActivity.java
@@ -1,14 +1,41 @@
 package com.unihyr.Unihyr;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
+    private NavController navController;
+    private NavigationView navigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialToolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout=findViewById(R.id.drawer_layout);
+        navigationView=findViewById(R.id.nav_view);
+        navController= Navigation.findNavController(this,R.id.nav_host_fragment);
+
+        AppBarConfiguration appBarConfiguration =
+                new AppBarConfiguration.Builder(navController.getGraph())
+                        .setDrawerLayout(drawerLayout)
+                        .build();
+
+        NavigationUI.setupWithNavController(toolbar,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
 }
