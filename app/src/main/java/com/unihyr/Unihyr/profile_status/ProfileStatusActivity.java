961ebaa290d651a58475a286fb28b37e70c6b817
Divij Gupta <divijgupta66@gmail.com>
2019-12-17 16:11:54 +0530

    profile status initial

---
+++ app/src/main/java/com/unihyr/Unihyr/profile_status/ProfileStatusActivity.java
@@ -0,0 +1,16 @@
+package com.unihyr.Unihyr.profile_status;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.unihyr.Unihyr.R;
+
+public class ProfileStatusActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_status);
+    }
+}
