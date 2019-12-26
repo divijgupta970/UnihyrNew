f1afe9435586d557cb1987d2b81281b9014d0d2e
Dell <nhikhil.nagpal31@rediffmail.com>
2019-12-26 14:47:46 +0530

    drives fragment must be completed

--- app/src/main/java/com/unihyr/Unihyr/calendar/CalendarFragment.java
+++ app/src/main/java/com/unihyr/Unihyr/calendar/CalendarFragment.java
@@ -3,13 +3,20 @@ package com.unihyr.Unihyr.calendar;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.CalendarMode;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
 import com.unihyr.Unihyr.R;
+
+import java.util.Calendar;
 
 
 /**
@@ -17,7 +24,7 @@ import com.unihyr.Unihyr.R;
  */
 public class CalendarFragment extends Fragment {
 
-
+    MaterialCalendarView materialCalendarView;
     public CalendarFragment() {
         // Required empty public constructor
     }
@@ -29,5 +36,15 @@ public class CalendarFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calendar, container, false);
     }
-
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState)
+    {
+        super.onViewCreated(view,savedInstanceState);
+        materialCalendarView=view.findViewById(R.id.calendarView);
+        materialCalendarView.state().edit()
+                .setFirstDayOfWeek(Calendar.MONDAY)
+                .setCalendarDisplayMode(CalendarMode.MONTHS)
+                .commit();
+        materialCalendarView.setSelectedDate(CalendarDay.today());
+    }
 }
