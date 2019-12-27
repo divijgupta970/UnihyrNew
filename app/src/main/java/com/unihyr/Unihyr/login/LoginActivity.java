47ffa2d223598adc415f07657deaccad41177eda
Divij Gupta <divijgupta66@gmail.com>
2019-12-27 15:16:56 +0530

    added shared element transition

--- app/src/main/java/com/unihyr/Unihyr/login/LoginActivity.java
+++ app/src/main/java/com/unihyr/Unihyr/login/LoginActivity.java
@@ -1,10 +1,12 @@
 package com.unihyr.Unihyr.login;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.OvershootInterpolator;
 
 import com.unihyr.Unihyr.MainActivity;
 import com.unihyr.Unihyr.R;
@@ -15,6 +17,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ConstraintLayout constraintLayout=findViewById(R.id.constraintLayout);
+        constraintLayout.setAlpha(0);
+        constraintLayout.animate()
+                .setStartDelay(500)
+                .alpha(1)
+                .start();
         findViewById(R.id.btnLogin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -22,5 +30,10 @@ public class LoginActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public void onBackPressed() {
+        finish();
     }
 }
--- app/src/main/java/com/unihyr/Unihyr/login/SplashActivity.java
+++ app/src/main/java/com/unihyr/Unihyr/login/SplashActivity.java
@@ -1,38 +1,57 @@
 package com.unihyr.Unihyr.login;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityOptionsCompat;
+import androidx.core.util.Pair;
 
-import com.unihyr.Unihyr.MainActivity;
 import com.unihyr.Unihyr.R;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ImageView ivLogo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         Animation animation = AnimationUtils.loadAnimation(this, R.anim.logo_fade_in);
-        findViewById(R.id.ivLogo).startAnimation(animation);
-        findViewById(R.id.tvLabel).startAnimation(animation);
-        final Intent intent = new Intent(this, LoginActivity.class);
-        Thread timer = new Thread() {
-            public void run() {
-                try {
-                    sleep(2500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                } finally {
-                    startActivity(intent);
-                    finish();
-                }
+        ivLogo=findViewById(R.id.ivLogo);
+        ivLogo.startAnimation(animation);
+        findViewById(R.id.tvLabel).startAnimation(AnimationUtils.loadAnimation(this,R.anim.logo_fade_in));
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
             }
-        };
-        timer.start();
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                animationEndFunction();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
+
+    private void animationEndFunction() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        String imageTransition = getString(R.string.transition_image);
+        Pair<View, String> p1 = Pair.create((View) ivLogo, imageTransition);
+
+        ActivityOptionsCompat options = ActivityOptionsCompat.makeSceneTransitionAnimation((Activity) SplashActivity.this, p1);
+        startActivity(intent, options.toBundle());
+        finish();
+    }
+
 }
