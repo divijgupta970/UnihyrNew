cf9b97850512e44ea717803bcaf58254d573b45f
Dell <nhikhil.nagpal31@rediffmail.com>
2019-12-30 13:02:43 +0530

    only java code left

--- app/src/main/java/com/unihyr/Unihyr/profile_status/ScheduleInterviewActivity.java
+++ app/src/main/java/com/unihyr/Unihyr/profile_status/ScheduleInterviewActivity.java
@@ -2,7 +2,10 @@ package com.unihyr.Unihyr.profile_status;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +18,30 @@ import com.unihyr.Unihyr.R;
 import java.util.ArrayList;
 
 public class ScheduleInterviewActivity extends AppCompatActivity {
+    protected void sendEmail() {
+        Log.i("Send email", "");
+        String[] TO = {
+                "nikhil.nagpal98@gmail.com"
+        };
+        String[] CC = {
+                "karamjitverma89@gmail.com"
+        };
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setData(Uri.parse("mailto:"));
+        emailIntent.setType("text/plain");
+        emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+        emailIntent.putExtra(Intent.EXTRA_CC, CC);
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+        emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+        try {
+            startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+            finish();
+            Log.i("Finished sending email...", "");
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(this, "There is no email client installed.", Toast.LENGTH_SHORT).show();
+        }
+    }
+    // This is just a sample script. Paste your real code (javascript or HTML) here.
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +52,7 @@ public class ScheduleInterviewActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 // on click to sned mail
+                sendEmail();
             }
         });
        Spinner rounds = (Spinner) findViewById(R.id.sc_Rounds);
